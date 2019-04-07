d1e613fdf11673a4520f86c9b1d0cae3907084e5
thydaduong <maduogrv@gmail.com>
2019-04-07 12:03:43 +0700

    first commit

---
+++ src/views/Canceled.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>CANCELED</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style>
+.home{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgray;
+  min-height: calc(100vh - 10rem)
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+
+        <button id="checkout-button" role="link" @click="stripePay">Pay</button>
+        <div id="error-message">{{errorMessage}}</div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+    components: {},
+    data(){
+      return{
+        errorMessage:""
+      }
+    },
+    created() {
+        console.log(window);
+    },
+    methods: {
+        stripePay() {
+          console.log("clicked");
+          
+            const stripe = window.Stripe(
+                "pk_test_PhVTXCyQHFddlKPzdQjiP9QU00HTk5E4iw",
+                {
+                    betas: ["checkout_beta_4"]
+                }
+            );
+
+            stripe
+                .redirectToCheckout({
+                    items: [{ sku: "sku_Eq87qQxHLP3qww", quantity: 1 }],
+                    successUrl: "http://localhost:8080/succeed",
+                    cancelUrl: "http://localhost:8080/canceled"
+                })
+                .then(function(result) {
+                  console.log(result);
+                  
+                    if (result.error) {
+                        // If `redirectToCheckout` fails due to a browser or network
+                        // error, display the localized error message to your customer.
+                        this.errorMessage = result.error.message;
+                    }
+                });
+        }
+    }
+};
 </script>
+
+<style>
+.home {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: lightgray;
+    min-height: calc(100vh - 10rem);
+}
+</style>
+
